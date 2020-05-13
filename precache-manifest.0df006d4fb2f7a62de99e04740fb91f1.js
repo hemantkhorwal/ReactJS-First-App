73d7507a25586b252ff4a717ae3c3ca66bdb2e16
hemu18th <hemantkhorwalhk@gmail.com>
2020-05-13 07:58:42 +0530

    Updates

---
+++ precache-manifest.0df006d4fb2f7a62de99e04740fb91f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b703ee2e4791ee364565aced749ad5",
+    "url": "/ReactJs-First-App/index.html"
+  },
+  {
+    "revision": "c393ffef57b39d9fe01c",
+    "url": "/ReactJs-First-App/static/css/main.79074318.chunk.css"
+  },
+  {
+    "revision": "5a976fc2c3eaa9c331ae",
+    "url": "/ReactJs-First-App/static/js/2.3cd13bf0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJs-First-App/static/js/2.3cd13bf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c393ffef57b39d9fe01c",
+    "url": "/ReactJs-First-App/static/js/main.a0b6e0cf.chunk.js"
+  },
+  {
+    "revision": "2be4431218ee30781e8b",
+    "url": "/ReactJs-First-App/static/js/runtime-main.b1732409.js"
+  }
+]);
